b8351a433ffc6e36e6c6cdb727f34a9e10e79015
lykemike <61860743+lykemike@users.noreply.github.com>
2020-12-15 11:34:44 +0800

    test commit 2

--- Pomodoro/src/Main.java
+++ Pomodoro/src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public Main() {
 		// TODO Auto-generated constructor stub
 		System.out.println("Pomodoro Project");
+		System.out.println("TEST 2");
+		System.out.println("TEST 3");
 	}
 
 	public static void main(String[] args) {
