f7916f172686d3b88bb63f95e6c7fa714d01206c
binggo345 <edrick930@gmail.com>
2021-01-30 00:42:15 +0700

    bug fixes buat stats

--- Pomodoro/src/CSV_Controller/FindWorkReader.java
+++ Pomodoro/src/CSV_Controller/FindWorkReader.java
@@ -4,32 +4,32 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class FindLast {
+public class FindWorkReader {
 	ArrayList<String> dayValArr = new ArrayList<String>();
 
-	public FindLast() {
+	public FindWorkReader() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public ArrayList<String> found() {
-		String line = "";
+		String line, SParsedLine;
+		Date parsedLine;
 		
 		try {
-			SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+			SimpleDateFormat dayName = new SimpleDateFormat("EEEE");
 			BufferedReader br = new BufferedReader(new FileReader("log.csv"));
 			
 			while((line = br.readLine()) != null) {
-
+				
 				if(line.contains("Work")) {
-					Date todayDay = new Date();
-					
-					if(line.contains(dateFormatter.format(todayDay))){
-						dayValArr.add(line);
-					}
+					parsedLine = new SimpleDateFormat("yyyy-MM-dd").parse(line);
+					SParsedLine = dayName.format(parsedLine);
+					dayValArr.add(SParsedLine);
 				}
 			}
 			
@@ -37,7 +37,10 @@ public class FindLast {
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}catch (IOException e) {
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
--- Pomodoro/src/Stat_Controller/UpdateStats.java
+++ Pomodoro/src/Stat_Controller/UpdateStats.java
@@ -4,42 +4,38 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-import CSV_Controller.FindLast;
+import CSV_Controller.FindWorkReader;
 
 public class UpdateStats {
 	private ArrayList<String> dayValArr;
 	private int[] dayVal;
-	
-	Date todayDay = new Date();
-	SimpleDateFormat dayName = new SimpleDateFormat("EEEE");
 
 	public UpdateStats() {
 		// TODO Auto-generated constructor stub
 	}
 
 	public int[] updateDayVal() {
-		FindLast fl = new FindLast();
+		FindWorkReader fl = new FindWorkReader();
 		dayValArr = fl.found();
 		dayVal = new int[7];
 		
 		for (String dv : dayValArr) {
-			if (dayName.format(todayDay).equals("Monday")) {
+			if (dv.equals("Monday")) {
 				dayVal[6]++;
-			}else if (dayName.format(todayDay).equals("Tuesday")) {
+			}else if (dv.equals("Tuesday")) {
 				dayVal[0]++;
-			}else if (dayName.format(todayDay).equals("Wednesday")) {
+			}else if (dv.equals("Wednesday")) {
 				dayVal[1]++;
-			}else if (dayName.format(todayDay).equals("Thursday")) {
+			}else if (dv.equals("Thursday")) {
 				dayVal[2]++;
-			}else if (dayName.format(todayDay).equals("Friday")) {
+			}else if (dv.equals("Friday")) {
 				dayVal[3]++;
-			}else if (dayName.format(todayDay).equals("Saturday")) {
+			}else if (dv.equals("Saturday")) {
 				dayVal[4]++;
-			}else if (dayName.format(todayDay).equals("Sunday")) {
+			}else if (dv.equals("Sunday")) {
 				dayVal[5]++;
 			}
-		}
-		
+		}	
 		return dayVal;
 	}
 }
